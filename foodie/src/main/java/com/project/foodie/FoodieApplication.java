e498016f5d692278693d92a1353cd9b6a13c93de
mansimm <mansimisal88@gmail.com>
2021-03-31 10:51:30 +0530

    initial commit

---
+++ foodie/src/main/java/com/project/foodie/FoodieApplication.java
@@ -0,0 +1,13 @@
+package com.project.foodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieApplication.class, args);
+	}
+
+}
---
+++ foodie/src/test/java/com/project/foodie/FoodieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.foodie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
