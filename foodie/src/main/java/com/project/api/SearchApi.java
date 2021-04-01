5c9974fdf162fab566bd30b69cd54281c703729e
mansimm <mansimisal88@gmail.com>
2021-04-01 14:33:52 +0530

    Exception handeling done for SearchApi.java class

--- foodie/src/main/java/com/project/api/SearchApi.java
+++ foodie/src/main/java/com/project/api/SearchApi.java
@@ -3,12 +3,14 @@ package com.project.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.model.RestaurantDto;
 import com.project.service.SearchServiceImpl;
@@ -21,12 +23,21 @@ public class SearchApi {
 	@Autowired
 	SearchServiceImpl search;
 	
+	@Autowired
+	Environment environment;
+	
 	@GetMapping(value="/getAllRestaurants")
 	public ResponseEntity<List<RestaurantDto>> getAllRestaurants()
 	{
-		System.out.println("in api class");
-		List<RestaurantDto> res = search.getAllRestaurant();
-		return new ResponseEntity<List<RestaurantDto>>(res,HttpStatus.OK);
+		try
+		{
+			List<RestaurantDto> res = search.getAllRestaurant();
+			return new ResponseEntity<List<RestaurantDto>>(res,HttpStatus.OK);
+		}
+		catch(Exception e){
+
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, environment.getProperty(e.getMessage()));
+		}
 	}
 	
 }
