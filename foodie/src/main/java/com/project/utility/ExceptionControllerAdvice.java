5ed7439bba9eb7055b2e0b190187c5a050eb67f9
mansimm <mansimisal88@gmail.com>
2021-04-03 21:48:49 +0530

    NoSuchAlgorithmException exception handler added in utility package

--- foodie/src/main/java/com/project/utility/ExceptionControllerAdvice.java
+++ foodie/src/main/java/com/project/utility/ExceptionControllerAdvice.java
@@ -30,13 +30,19 @@ public class ExceptionControllerAdvice {
 		return new ResponseEntity<ErrorInfo>(error, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	@ExceptionHandler(NoSuchAlgorithmException.class)
+	public ResponseEntity<ErrorInfo> exceptionHandler(NoSuchAlgorithmException exception) {
+		ErrorInfo error = new ErrorInfo();
+		error.setErrorMessage(environment.getProperty("General.EXCEPTION_MESSAGE"));
+		error.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		error.setTimestamp(LocalDateTime.now());
+		return new ResponseEntity<ErrorInfo>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 	@ExceptionHandler(UserServiceException.class)
 	public ResponseEntity<ErrorInfo> infyBankexceptionHandler(UserServiceException exception) {
 		ErrorInfo error = new ErrorInfo();
 		error.setErrorMessage(environment.getProperty(exception.getMessage()));
-		//System.out.println(exception.getMessage());
-		//System.out.println(environment.getProperty(exception.getMessage()));
-		
 		error.setTimestamp(LocalDateTime.now());
 		error.setErrorCode(HttpStatus.NOT_FOUND.value());
 		return new ResponseEntity<ErrorInfo>(error, HttpStatus.NOT_FOUND);
